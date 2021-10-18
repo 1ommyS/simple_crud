3fb99bce3143d66db6279e68d1b366b363d1f4c9
1ommy <santiago_bravo@mail.ru>
2021-10-18 19:43:12 +0300

    replace for i to filter

--- src/main/java/com/example/simplecrud/service/ProductService.java
+++ src/main/java/com/example/simplecrud/service/ProductService.java
@@ -11,11 +11,11 @@ import java.util.stream.Collectors;
 @Service
 public class ProductService {
 
-    private  List<Product> productList;
+    private List<Product> productList;
 
     public ProductService() {
         this.productList = new ArrayList<>() {{
-            add(new Product("Iphone 13", 80000));
+            add(new Product("Iphone", 80000));
             add(new Product("Samsung Galaxy S21", 120000));
             add(new Product("One Plus 9PRO", 55000));
         }};
@@ -30,19 +30,11 @@ public class ProductService {
     }
 
     public void updateProduct(String name, Product newProduct) throws ProductNotFoundException {
-        int index = -1;
-        for (int i = 0; i < productList.size(); i++) {
-            if (productList.get(i).getName().equals(name)) {
-                index = i;
-                break;
-            }
-        }
-        if (index == -1) {
-            throw new ProductNotFoundException("Product with name " + name + " wasn't found");
-        }
+        var product = productList.stream().filter(p ->p.getName().equals(name)).findFirst().orElseThrow(ProductNotFoundException::new);
 
-        productList.get(index).setName(newProduct.getName()).setPrice(newProduct.getPrice());
+        product.setPrice(newProduct.getPrice()).setName(newProduct.getName());
     }
+
 
     public void deleteProduct(String name) {
         productList = productList.stream().filter(p -> !p.getName().equals(name)).collect(Collectors.toList());
